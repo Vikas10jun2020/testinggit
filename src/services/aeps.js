0edb93a6f9ff7cb0dd1254c613c7bdc5c5765668
vikas <vikassoni247@gmail.com>
2023-10-16 09:34:59 +0530

    working on AEPS apis

---
+++ src/services/aeps.js
@@ -0,0 +1,11 @@
+const aepsKYC = async object => {};
+
+const aepsBanks = async object => {};
+
+const aepsTransaction = async object => {};
+
+const aepsCheck2Factor = async object => {};
+
+const aeps2FactorRegistration = async object => {};
+
+const aeps2FactorAuthentication = async object => {};
